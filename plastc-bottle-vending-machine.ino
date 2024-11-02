1bf88326c8862590c034e9bc75b43120833ce47d
Leo <leojunebedeo@gmail.com>
2024-11-02 15:08:29 +0800

    Add dispense code

--- plastc-bottle-vending-machine.ino
+++ plastc-bottle-vending-machine.ino
@@ -1,53 +1,84 @@
 #include <EEPROM.h>
 #include <LiquidCrystal_I2C.h>
 #include <Servo.h>
+#include <HCSR04.h>
+#include <EasyButton.h>
+#include <TaskScheduler.h>
 
 LiquidCrystal_I2C lcd(0x27,16,2);
+Scheduler runner;
 
-Servo myservo;
+Servo dispense_bottle_servo;
+Servo dispense_item1_servo;
+Servo dispense_item2_servo;
 
-int servo_position = 0;
-
-#define SERVO_PIN 9
-#define PLASTIC_SENSOR_PIN 7
-#define METAL_SENSOR_PIN 8
+#define PLASTIC_SENSOR_PIN 2
+#define METAL_SENSOR_PIN 3
+#define ITEM1_BUTTON_PIN 4
+#define ITEM2_BUTTON_PIN 5
+#define IR1_PIN 6
+#define IR2_PIN 7
+#define DISPENSE_BOTTLE_SERVO_PIN 8
+#define DISPENSE_ITEM1_SERVO_PIN 9
+#define DISPENSE_ITEM2_SERVO_PIN 10
 
 // Ultrasonic Sensor Pins
-#define TRIG_PIN_1 11
-#define ECHO_PIN_1 12
+#define TRIG_PIN 11
+#define ECHO_PIN 12
 
-float temp_In_C = 20.0;     // Can enter actual air temp here for maximum accuracy
-float speed_Of_Sound;       // Calculated speed of sound based on air temp
-float distance_Per_uSec;    // Distance sound travels in one microsecond
-float max_distance = 150;   // In centimeter
+HCSR04 hc(TRIG_PIN, ECHO_PIN);
 
+EasyButton item1_button(ITEM1_BUTTON_PIN);
+EasyButton item2_button(ITEM2_BUTTON_PIN);
+
+int bottle_count_address = 0;
 int bottle_count = 0;
+
+int item1_price = 3;
+int item2_price = 3;
+
+const long interval = 10000;
+
+void homeScreen();
+
+Task taskHomeScreen(100, TASK_FOREVER, &homeScreen);
 
 void setup() {
     Serial.begin(9600);
     
     pinMode(PLASTIC_SENSOR_PIN, INPUT);
     pinMode(METAL_SENSOR_PIN, INPUT);
-    pinMode(TRIG_PIN_1, OUTPUT);
-    pinMode(ECHO_PIN_1, INPUT);
+    pinMode(IR1_PIN, INPUT_PULLUP);
+    pinMode(IR2_PIN, INPUT_PULLUP);
 
-    myservo.attach(SERVO_PIN);
-    myservo.write(90);
-
-    // Formula to calculate speed of sound in meters/sec based on temp
-    speed_Of_Sound = 331.1 +(0.606 * temp_In_C);  
-    // Calculate the distance that sound travels in one microsecond in centimeters
-    distance_Per_uSec = speed_Of_Sound / 10000.0;
+    dispense_bottle_servo.attach(DISPENSE_BOTTLE_SERVO_PIN);
+    dispense_bottle_servo.write(90);
+    delay(1000);
+    dispense_bottle_servo.detach();
 
     lcd.init();
     lcd.backlight();
 
-    // EEPROM.put(0, 0);
-    EEPROM.get(0, bottle_count);
+    // EEPROM.put(bottle_count_address, 0);
+    EEPROM.get(bottle_count_address, bottle_count);
+
+    item1_button.begin();
+    item2_button.begin();
+    item1_button.onPressed(dispenseItem1);
+    item2_button.onPressed(dispenseItem2);
+
+    runner.addTask(taskHomeScreen);
+
+    taskHomeScreen.enable();
 }
 
 void loop() {
-  // Display bottle count 
+  runner.execute();
+  item1_button.read();
+  item2_button.read();
+}
+
+void homeScreen() {
   lcd.setCursor(0,1);
   lcd.print("Bottle count: ");
   lcd.setCursor(14,1);
@@ -58,8 +89,8 @@ void loop() {
 
 void check_cylinder_for_bottle() {
     
-    float cylinder_ultrasonic_distance = read_sensor("in cylinder", TRIG_PIN_1, ECHO_PIN_1);
-
+    float cylinder_ultrasonic_distance = hc.dist();
+    
     int plastic_sensor_reading = digitalRead(PLASTIC_SENSOR_PIN);
     int metal_sensor_reading = digitalRead(METAL_SENSOR_PIN);
     Serial.println(plastic_sensor_reading);
@@ -68,37 +99,37 @@ void check_cylinder_for_bottle() {
     if (cylinder_ultrasonic_distance < 10) {
 
         lcd.setCursor(0,0);
-        lcd.print("Analyzing object");
+        lcd.print("Analyzing bottle");
 
-        delay(1000);
+        delay(2000);
     
-        // 0 = Detected, 1 = No Detected
+        // 0 = Detected, 1 = Non Detected
         if (plastic_sensor_reading == 0 & metal_sensor_reading == 1) {
             // Plastic reading
             lcd.setCursor(0,0);
             lcd.print("Plastic found   ");
             lcd.setCursor(0,1);
-            lcd.print("Disposing       ");
+            lcd.print("Disposing...       ");
+
+            bottle_count = bottle_count + 1;
+            EEPROM.put(bottle_count_address, bottle_count);
             
             do {
                 turn_servo_to_plastic();
                 delay(1000);
-                cylinder_ultrasonic_distance = read_sensor("in cylinder", TRIG_PIN_1, ECHO_PIN_1);
+                cylinder_ultrasonic_distance = hc.dist();
             } while(cylinder_ultrasonic_distance < 10);
-
-            bottle_count = bottle_count + 1;
-            EEPROM.put(0, bottle_count);
         } else {
             // Non-plastic reading        
             lcd.setCursor(0,0);
             lcd.print("Nonplastic found");
             lcd.setCursor(0,1);
-            lcd.print("Disposing       ");
+            lcd.print("Disposing...       ");
             
             do {
                 turn_servo_to_non_plastic();
                 delay(1000);
-                cylinder_ultrasonic_distance = read_sensor("in cylinder", TRIG_PIN_1, ECHO_PIN_1);
+                cylinder_ultrasonic_distance = hc.dist();
             } while(cylinder_ultrasonic_distance < 10);
         }
     } else {
@@ -112,68 +143,120 @@ void check_cylinder_for_bottle() {
     }
 }
 
-float read_sensor(String sensor_name, int TRIG_PIN, int ECHO_PIN) {
-    float duration, distanceCm, final_distance=0;
-  
-    for (int i=0; i<2; i++) {
-        digitalWrite(TRIG_PIN, HIGH);       // Set trigger pin HIGH 
-        delayMicroseconds(50);              // Hold pin HIGH for 20 uSec
-        digitalWrite(TRIG_PIN, LOW);        // Return trigger pin back to LOW again.
-        duration = pulseIn(ECHO_PIN, HIGH);  // Measure time in uSec for echo to come back.
-       
-        // convert the time data into a distance in centimeters, inches and feet
-        duration = duration / 2.0;  // Divide echo time by 2 to get time in one direction
-        distanceCm = duration * distance_Per_uSec;
-    
-        if (distanceCm > final_distance) {
-            if (distanceCm > max_distance) {
-                final_distance = max_distance;
-            } else {
-                final_distance = distanceCm;
-            }
-        }
-    
-        delay(10);
-    }
-
-    if (sensor_name != "") {
-        if (final_distance <= 0){
-            Serial.println("Sensor " + sensor_name +  " out of range");
-        } else {
-            Serial.print("Sensor " + sensor_name + ": ");
-            Serial.print(distanceCm, 0);
-            Serial.print("cm");
-            Serial.println();
-        }
-    }
-  
-    return distanceCm;
-}
-
 void turn_servo_to_plastic() {
-    for (servo_position = 90; servo_position <= 180; servo_position += 1) {
-        myservo.write(servo_position);
+    dispense_bottle_servo.attach(DISPENSE_BOTTLE_SERVO_PIN);
+    for (int pos = 90; pos <= 180; pos += 1) {
+        dispense_bottle_servo.write(pos);
         delay(10);
     }
 
     delay(1000);
 
-    for (servo_position = 180; servo_position >= 90; servo_position -= 1) {
-        myservo.write(servo_position);
+    for (int pos = 180; pos >= 90; pos -= 1) {
+        dispense_bottle_servo.write(pos);
         delay(10);
     }
+    dispense_bottle_servo.detach();
 }
 
 void turn_servo_to_non_plastic() {
-    for (servo_position = 90; servo_position >= 0; servo_position -= 1) {
-        myservo.write(servo_position);
+    dispense_bottle_servo.attach(DISPENSE_BOTTLE_SERVO_PIN);
+    for (int pos = 90; pos >= 0; pos -= 1) {
+        dispense_bottle_servo.write(pos);
         delay(10);
     }
 
     delay(1000);
 
-    for (servo_position = 0; servo_position <= 90; servo_position += 1) {
-        myservo.write(servo_position);
+    for (int pos = 0; pos <= 90; pos += 1) {
+        dispense_bottle_servo.write(pos);
         delay(10);
+    }
+    dispense_bottle_servo.detach();
+}
+
+void dispenseItem1() {
+    if ((bottle_count - item1_price) < 0) {
+        lcd.setCursor(0,0);
+        lcd.print("Need more bottle");
+        delay(3000);
+    } else {
+        lcd.setCursor(0,0);
+        lcd.print("Item #1 selected");
+        lcd.setCursor(0,1);
+        lcd.print("Please wait...  ");
+        
+        unsigned long startMillis = millis();
+        bool timedOut = false;
+    
+        // Start servo
+        dispense_item1_servo.attach(DISPENSE_ITEM1_SERVO_PIN);
+        dispense_item1_servo.write(120);
+
+        // Wait item to drop
+        while (digitalRead(IR1_PIN) == 1) {
+          if (millis() - startMillis >= interval) {
+              timedOut = true;
+              break;
+          }
+        }
+
+        // Stop servo
+        dispense_item1_servo.write(90);
+        dispense_item1_servo.detach();
+    
+        if (timedOut == false) {
+            bottle_count = bottle_count - item1_price;
+            EEPROM.put(bottle_count_address, bottle_count);
+    
+            lcd.setCursor(0,0);
+            lcd.print("Thank you!      ");
+            lcd.setCursor(0,1);
+            lcd.print("                ");
+            delay(3000);
+        }
+    }
+}
+
+void dispenseItem2() {
+    if ((bottle_count - item2_price) < 0) {
+        lcd.setCursor(0,0);
+        lcd.print("Need more bottle");
+        delay(3000);
+    } else {
+        lcd.setCursor(0,0);
+        lcd.print("Item #2 selected");
+        lcd.setCursor(0,1);
+        lcd.print("Please wait...  ");
+        
+        unsigned long startMillis = millis();
+        bool timedOut = false;
+    
+        // Start servo
+        dispense_item2_servo.attach(DISPENSE_ITEM2_SERVO_PIN);
+        dispense_item2_servo.write(120);
+
+        // Wait item to drop
+        while (digitalRead(IR2_PIN) == 1) {
+          if (millis() - startMillis >= interval) {
+              timedOut = true;
+              break;
+          }
+        }
+
+        // Stop servo
+        dispense_item2_servo.write(90);
+        dispense_item2_servo.detach();
+    
+        if (timedOut == false) {
+            bottle_count = bottle_count - item2_price;
+            EEPROM.put(bottle_count_address, bottle_count);
+    
+            lcd.setCursor(0,0);
+            lcd.print("Thank you!      ");
+            lcd.setCursor(0,1);
+            lcd.print("                ");
+            delay(3000);
+        }
     }
 }
